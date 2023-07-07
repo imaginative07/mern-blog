c93c0e178b48bf56047fe3eb1cdd0505c5089ae6
Imaginative <imaginative07@gmail.com>
2023-07-08 02:29:42 +0530

    db.js file added to add the database function and server file updated

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,31 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import ConnectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
+// Load env variables
 dotenv.config();
 
-// Define port to run the server
+// Connect to Database
+ConnectDB(); 
+
+// Server listen port
 const port = process.env.PORT || 5000;
 
 // Initiate express 
 const app = express();
+
+// logging middleware
+var num = 0;
+app.use(function (req, res, next) {
+    var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    var method = req.method;
+    var url = req.url;
+
+    console.log('\n- - - - - - - - - - - -\n'+(++num) + ". IP " + ip + " " + method + " " + url+'\n- - - - - - - - - - - -\n');
+    next();
+});
 
 app.use('/', (req, res) => {
     res.send('Welcome to MERN Blog');
