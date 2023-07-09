c0fcee36ea7b9b7d804ad7a78adddc7b125d1c04
Imaginative <imaginative07@gmail.com>
2023-07-10 01:51:47 +0530

    Cleaned up the app.js and index.js and added react router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,20 @@
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Header from './components/Header';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ToastContainer />
+      <Header />
+      <main className="py-3">
+        <Container>
+            <Outlet />
+        </Container>
+      </main>
+    </>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { createBrowserRouter, createRoutesFromChildren, Route, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
 import reportWebVitals from './reportWebVitals';
+
+const router = createBrowserRouter(
+  createRoutesFromChildren(
+    <Route path="/" element={<App />}>
+      <Route index={true} path='/' element={<Home />} />
+    </Route>
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
