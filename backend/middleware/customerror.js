d0fe2e5ce3ec0b9fb9f951c86f08f5a3f952cc5f
Imaginative <imaginative07@gmail.com>
2023-07-11 14:21:28 +0530

    Fix the redeclare variable error by changing const to let

--- backend/middleware/customerror.js
+++ backend/middleware/customerror.js
@@ -5,7 +5,7 @@ const notFound = (req, res, next) => {
 };
 
 const errorHandler = (err, req, res, next) => {
-    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
     let message = err.message;
 
     // Check for mongoose bad objectID
