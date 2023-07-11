aad5eb3bdd56cb553fba99caab575f1f4c2794e4
Imaginative <imaginative07@gmail.com>
2023-07-11 14:22:33 +0530

    Footer component added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <main>
           <Outlet />
       </main>
+      <Footer />
     </>
   );
 }
